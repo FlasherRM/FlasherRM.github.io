f88581f65e403b8d127cac3aebfb7542ff8e03dd
Roman <romanmmmelnyk@gmail.com>
2022-05-04 21:30:29 +0300

    last

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-
+        
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
@@ -14,21 +14,25 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
+
     // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
+
     $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
+
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
@@ -36,12 +40,14 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
+
     var typed = new Typed(".typing-2", {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
+
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
